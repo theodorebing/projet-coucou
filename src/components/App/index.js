8d1ee8edb8a43c6f335219320425f355bf37432b
Théodore Bing <theodorebing@gmail.com>
2021-03-03 12:08:58 +0100

    components commented

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 // == Import
 import './styles.scss';
-import Menu from 'src/components/Menu';
-import Content from 'src/components/Content';
+// import Menu from 'src/components/Menu';
+// import Content from 'src/components/Content';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <Menu />
-    <Content />
+    {/* <Menu />
+    <Content /> */}
   </div>
 );
 
