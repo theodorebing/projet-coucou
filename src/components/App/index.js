3f05fd85564a34fb250cc927f4f6d777852aca91
seb6634 <gonzalez.sebastien66@gmail.com>
2021-03-03 15:59:18 +0100

    login / signin component of the home page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,15 +2,12 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
 import './styles.css';
+import IndexLoginForm from 'src/components/Form/IndexLoginForm'
 
 // == Composant
 const App = () => (
-  <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
-  </div>
+  <IndexLoginForm/>
 );
 
 // == Export
