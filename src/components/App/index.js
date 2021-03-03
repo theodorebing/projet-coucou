2a7a0a5db553664adbf789abb400fe33a1ec8281
seb6634 <gonzalez.sebastien66@gmail.com>
2021-03-03 21:12:18 +0100

    component footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,12 +5,15 @@ import React from 'react';
 import './styles.scss';
 // import Menu from 'src/components/Menu';
 // import Content from 'src/components/Content';
+ import Footer from 'src/components/Footer';
+
 
 // == Composant
 const App = () => (
   <div className="app">
     {/* <Menu />
     <Content /> */}
+    <Footer/>
   </div>
 );
 
