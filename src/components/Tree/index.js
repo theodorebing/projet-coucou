ab3de12affe51f1d599e6cc068050da9441eda19
seb6634 <gonzalez.sebastien66@gmail.com>
2021-03-10 14:59:06 +0100

    tree-v3

--- src/components/Tree/index.js
+++ src/components/Tree/index.js
@@ -1,62 +1,86 @@
-import { number } from "prop-types";
+
 import React from "react";
 import Tree from "react-d3-tree";
 
 const debugData = [
   {
-    name: "",
+    name: {
+      name: "1",
+      place_of_birth: 'Tourcoin',
+    person_UUID: '1477788895',
+    date_of_birth: '18/02/2021',
+    photo: 'url...',
+    last_name: 'coucou',
+    first_name: 'coucou',
+    last_known_location: 'Tourcoin'
+    },
+    
     children: [
       {
-        name: "1"
+        name: "1",
+        place_of_birth: 'Tourcoin',
+      person_UUID: '1477788895',
+      date_of_birth: '18/02/2021',
+      photo: 'url...',
+      last_name: 'coucou',
+      first_name: 'coucou',
+      last_known_location: 'Tourcoin'
       },
       {
-        name: "2"
+        name: "2",
+        place_of_birth: 'Tourcoin',
+        person_UUID: '1477788895',
+        date_of_birth: '18/02/2021',
+        photo: 'url...',
+        last_name: 'coucou',
+        first_name: 'coucou',
+        last_known_location: 'Tourcoin'
       },
       {
-        name: "3"
+        name: "3",
+        place_of_birth: 'Tourcoin',
+        person_UUID: '1477788895',
+        date_of_birth: '18/02/2021',
+        photo: 'url...',
+        last_name: 'coucou',
+        first_name: 'coucou',
+        last_known_location: 'Tourcoin'
       },
       {
-        name: "4"
+        name: "4", place_of_birth: 'Tourcoin',
+        person_UUID: '1477788895',
+        date_of_birth: '18/02/2021',
+        photo: 'url...',
+        last_name: 'coucou',
+        first_name: 'coucou',
+        last_known_location: 'Tourcoin'
       }
 
-    ]
+    ],
+    
   }
 ];
-
-
-// const debugData = [
-//   {
-//     name: "",
-//     children: [
-//       {
-//         name: "1"
-//       },
-//       {
-//         name: "2"
-//       },
-
-//     ]
-//   }
-// ];
 
 const containerStyles = {
   width: '100%',
   height: '100vh',
 }
-
-const foreignObjectProps = { width: 200, height: 200, x: -100, y: -20 };
+const foreignObjectProps = { width: 250, height: 250, x: -100, y: -20 };
 const renderForeignObjectNode = ({ nodeDatum, toggleNode, }) => {
 
   return (
     <g>
-      <foreignObject {...foreignObjectProps} >
-      <text fill="black" strokeWidth="1" >
-      {nodeDatum.name ? `Nom: ${nodeDatum.name}` : ''}
-    </text>
+      <foreignObject {...foreignObjectProps} >    
+       <ul className='information__person'>
+         <li>{nodeDatum.last_name ? `Nom: ${nodeDatum.last_name}` : ''}</li>
+         <li>{nodeDatum.first_name ? `Prénom: ${nodeDatum.first_name}` : ''}</li>
+         <li>{nodeDatum.date_of_birth ? `Anniversaire: ${nodeDatum.date_of_birth}` : ''}</li>
+         <li>{nodeDatum.last_known_location ? `Habite à: ${nodeDatum.last_known_location}` : ''}</li>
+       </ul>
         <img src="https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-11-avatar-2754576_120520.png"
           height='200'
           width='200'
-         />
+        />
       </foreignObject>
     </g>
   )
@@ -74,14 +98,13 @@ export default class CenteredTree extends React.PureComponent {
       }
     });
   }
-
   render() {
     const nodeSize = { x: 400, y: 400 };
 
     return (
       <div style={containerStyles} ref={tc => (this.treeContainer = tc)}>
         <Tree
-          zoomable={false}
+
           nodeSize={nodeSize}
           pathFunc={'step'}
           data={debugData}
@@ -95,58 +118,3 @@ export default class CenteredTree extends React.PureComponent {
     );
   }
 }
-
-// import React from "react";
-// import Tree from "react-d3-tree";
-// //import orgChartJson from "./data/org-chart.json";
-// import { useCallback, useState } from "react";
-// import "./styles.scss";
-
-// const containerStyles = {
-//   width: "100vw",
-//   height: "100vh"
-// };
-// const renderForeignObjectNode = ({
-//   nodeDatum,
-//   toggleNode,
-//   foreignObjectProps
-// }) => (
-//   <g>
-//     <foreignObject {...foreignObjectProps}>
-//       <div style={{ height:'40px',  border: "1px solid black", backgroundColor: "#dedede" }}>
-
-//       </div>
-//     </foreignObject>
-//   </g>
-// );
-
-// export default function App() {
-//   const [translate, containerRef] = useCenteredTree();
-//   const nodeSize = { x: 200, y: 200 };
-//   const foreignObjectProps = { width: nodeSize.x, height: nodeSize.y, x: 20 };
-//   return (
-//     <div style={containerStyles} >
-//       <Tree
-//         data={debugData}
-//         translate={translate}
-//         nodeSize={nodeSize}
-//         renderCustomNodeElement={(rd3tProps) =>
-//           renderForeignObjectNode({ ...rd3tProps, foreignObjectProps })
-//         }
-//         orientation="vertical"
-//       />
-//     </div>
-//   );
-// }
-
-
-// export const useCenteredTree = () => {
-//   const [translate, setTranslate] = useState({ x: 0, y: 0 });
-//   const containerRef = useCallback((containerElem) => {
-//     if (containerElem !== null) {
-//       const { width, height } = containerElem.getBoundingClientRect();
-//       setTranslate({ x: width / 2, y: height / 2 });
-//     }
-//   }, []);
-//   return [translate, containerRef];
-// };
