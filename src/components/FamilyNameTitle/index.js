67e536a4fda8e976fe6b466cb9ea9d16e2122a66
Théodore Bing <theodorebing@gmail.com>
2021-03-16 07:13:29 +0100

    family name is showing

--- src/components/FamilyNameTitle/index.js
+++ src/components/FamilyNameTitle/index.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import axios from 'src/api';
+import baseurl from 'src/middlewares/baseurl';
 
 import './styles.scss';
 
 // == Component
-const FamilyNameTitle = ({ familyName }) => (
-  <>
-    <h1 className="familyNameTitle">{familyName}</h1>
-  </>
-);
-
-FamilyNameTitle.propTypes = {
-  familyName: PropTypes.string,
-};
-
-FamilyNameTitle.defaultProps = {
-  familyName: 'familyName',
+const FamilyNameTitle = () => {
+  const [family, setFamily] = useState({});
+  (useEffect(() => {
+    axios.get(`${baseurl}family`).then((result) => {
+      if (result && result.data) {
+        // setTimeout(() => {
+        setFamily(result.data.family);
+        // }, 2000);
+      }
+    });
+  }, []));
+  return (
+    <>
+      <h1 className="familyNameTitle">{family.designation}</h1>
+    </>
+  );
 };
 
 // == Export
