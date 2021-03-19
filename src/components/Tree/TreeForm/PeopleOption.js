24b9e19c9675939474ced7e7a870f39719a03087
seb6634 <gonzalez.sebastien66@gmail.com>
2021-03-19 23:46:10 +0100

    information person of the tree and delete person

--- src/components/Tree/TreeForm/PeopleOption.js
+++ src/components/Tree/TreeForm/PeopleOption.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const PeopleOption = ({ id, firstName, lastName }) => {
-  console.log('people names', firstName, lastName);
   return (
     <>
       <option value={id}>{firstName} {lastName}</option>
