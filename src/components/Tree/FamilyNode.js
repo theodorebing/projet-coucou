7d451290ac736a509615b4fd7de9633de450daf2
seb6634 <gonzalez.sebastien66@gmail.com>
2021-03-19 14:27:32 +0100

    add tree gender picture

--- src/components/Tree/FamilyNode.js
+++ src/components/Tree/FamilyNode.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import avatar from 'src/assets/images/avatar.png';
+import avatarWoman from 'src/assets/images/avatar-woman.png';
+import avatarMan from 'src/assets/images/avatar-man.png';
+
 
 const FamilyNode = ({ node, style }) => {
-  console.log('node', node);
+  console.log('node', node.gender);
   return (
     <div style={style} className="person__tree">
-      <img
-        className="person__tree__picture"
-        src={avatar}
-        alt="profil"
-      />
+      {node.gender === 2 ? (
+        <>
+          <img
+            className="person__tree__picture"
+            src={avatarMan}
+            alt="profil"
+          />
+        </>
+      ) : (
+        <img
+          className="person__tree__picture"
+          src={avatarWoman}
+          alt="profil"
+        />
+      )}
       <div className="person__tree__name">
         {node.firstName} {node.lastName}
       </div>
