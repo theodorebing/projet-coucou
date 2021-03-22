e5ddc45acea0fdb4082ec1caed14e488cfb8338e
seb6634 <gonzalez.sebastien66@gmail.com>
2021-03-22 22:13:39 +0100

    adding tree zoom responsive

--- src/components/Tree/index.js
+++ src/components/Tree/index.js
@@ -13,8 +13,10 @@ import './styles.scss';
 
 const Tree = ({ noHelpButtons }) => {
   const [tree, setTree] = useState(null);
+  const [sizeScreenX, setSizeScreenX] = useState(null);
   useEffect(() => {
     noHelpButtons();
+    setSizeScreenX(screen.width)
     axios.get(`${baseurl}9/tree`)
       .then((result) => {
         if (result && result.data) {
@@ -25,17 +27,24 @@ const Tree = ({ noHelpButtons }) => {
         (console.log('cath tree', error));
       });
   }, []);
+  // distance between tree nodes
   const WIDTH = 350;
   const HEIGHT = 350;
+  // **************
   return (
     <div className="tree">
       {tree && Object.keys(tree).length ? (
         <>
           <FamilyNameTitle />
+          { sizeScreenX  < 800 ? (
           <TransformWrapper
-            defaultScale={1}
-            defaultPositionX={200}
-            defaultPositionY={150}
+            defaultScale={0.3}
+            options={{
+              minScale:0.3,
+              maxScale:2,
+              
+            }}
+            wheel={{ step: 50 }}
           >
             <TransformComponent>
               <ReactFamilyTree
@@ -64,6 +73,43 @@ const Tree = ({ noHelpButtons }) => {
               </div>
             </TransformComponent>
           </TransformWrapper>
+          ) : (
+            <TransformWrapper
+            defaultScale={1}
+            options={{
+              minScale:0.5,
+              maxScale:1.5
+            }}
+            wheel={{ step: 0.5 }}
+          >
+            <TransformComponent>
+              <ReactFamilyTree
+                nodes={tree.familyTree}
+                rootId={tree.rootId}
+                width={WIDTH}
+                height={HEIGHT}
+                renderNode={(node) => (
+                  <FamilyNode
+                    key={node.id}
+                    node={node}
+                    // name={node}
+                    style={{
+                      position: 'absolute',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      width: WIDTH,
+                      height: HEIGHT,
+                      transform: `translate(${node.left * (WIDTH / 2)}px, ${node.top * (HEIGHT / 2)}px)`,
+                    }}
+                  />
+                )}
+              />
+              <div className="plusButton-div">
+              </div>
+            </TransformComponent>
+          </TransformWrapper>
+          )}
         </>
       ) : (
         <h2 className="story-title">Loading</h2>
